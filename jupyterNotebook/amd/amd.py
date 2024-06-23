c8323ceef49d41d8ab2235906cc88645fb7490d6
´lrondan´ <lrondan1126@gmail.com>
2024-06-24 00:09:39 +0200

    five commit

--- jupyterNotebook/amd/amd.py
+++ jupyterNotebook/amd/amd.py
@@ -8,7 +8,7 @@ with open('amd/amd.json') as json_file:
     amd_info = json.load(json_file)
     # Print the type of data variable    
     print("Type:", type(amd_info))
-amd_info['country', 'sector']
+amd_info['country']
 
 amd_share_price_data = amd.history(period='max') #se convierte en un data frame
 print(amd_share_price_data.head(1)) #vizualiza la primera fila del Df
